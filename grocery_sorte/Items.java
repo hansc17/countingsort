0c3e252cc9ab9a569aed9300d04832e8c2dbdc9b
Jeovany <jeovanyguillen.j@gmail.com>
2021-10-18 20:10:29 -0700

    made the prototypr of the Items.java class

--- grocery_sorte/Items.java
+++ grocery_sorte/Items.java
@@ -2,13 +2,22 @@ package grocery_sorte;
 
 public class Items {
     String name; //name of item
-    int enterNode; //
-    int exitNode;
+    String section; //the section name
+    int enterNode; //where to enter
+    int exitNode; //where to exit
 
-    public Items(String name, int enterNode, int exitNode) {
+    public Items(String name, int enterNode, int exitNode, String section) {
         this.name = name;
         this.enterNode = enterNode;
         this.exitNode = exitNode;
+        this.section = section;
     }//end of Items constructor with 2 parameters
     
+    public void Details(){
+        System.out.println("\nItem: " +this.name);
+        System.out.println("Located between node: " + this.enterNode + " and " + this.exitNode);
+        System.out.println("Section: " + this.section);
+    }//end of Details() method
+
+
 }//end of Items class
